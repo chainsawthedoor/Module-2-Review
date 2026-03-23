fe3e40aad2cec7c8fb5f131541c2c44baa8c09eb
Jeffrey Gates <jeffreymrgates@gmail.com>
2026-03-23 13:03:25 -0400

    Created Class Student and Method AddGrade

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Module_2_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Module_2_Review;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public List<double> Grades { get; set; } = new List<double>();
+
+    public void AddGrade(double grade)
+    {
+        Grades.AddRange(grade);
+    }
+    
+    
+    
+}
